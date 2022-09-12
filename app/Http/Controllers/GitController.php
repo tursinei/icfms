d5a417d7c36c7395102e8464a81fad48b03df681
tursinei <vsina10@gmail.com>
2022-09-12 14:19:12 +0700

    replace \n to br

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -1,11 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-
+ 
 class GitController extends Controller
 {
     public function pull()
