9ae2d7725054b6a700948f974d8d4139083d25f5
tursinei <vsina10@gmail.com>
2022-09-12 14:22:58 +0700

    remove empty function

--- app/Http/Controllers/AnnouncementsController.php
+++ app/Http/Controllers/AnnouncementsController.php
@@ -75,30 +75,6 @@ class AnnouncementsController extends Controller
         $service = new AnnouncementService();
         $service->getFile($exp[0]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
     /**
      * Remove the specified resource from storage.
      *
